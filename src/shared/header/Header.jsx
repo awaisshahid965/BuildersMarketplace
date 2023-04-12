8d10f3faf179d7c152addde465db13c7f7137e75
Muhammad Awais <awaisshahid965@gmail.com>
2023-04-12 11:34:25 -0700

    add header bottom

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,48 +1,47 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import AppLogo from '../icons/AppLogo'
-import HeaderContactBar from './HeaderContactBar'
-import HeaderLink from './HeaderLink'
+import React from "react";
+import AppLogo from "../icons/AppLogo";
+import HeaderContactBar from "./HeaderContactBar";
+import HeaderLink from "./HeaderLink";
+import HeaderBottom from "./HeaderBottom";
 
 function Header() {
-  return (
-    <header className="header" data-header>
-        <HeaderContactBar />
+    return (
+        <header className="header" data-header>
+            <HeaderContactBar />
 
-    <div className="overlay" data-overlay></div>
+            <div className="overlay" data-overlay></div>
 
-    
-
-    <div className="header-bottom">
-      <div className="container">
-        <a href="/" className="logo">
-          <AppLogo />
-        </a>
-        <nav className="navbar" data-navbar>
-          <div className="navbar-top">
-            <a href="/" className="logo">
-              <AppLogo />
-            </a>
-            <button className="nav-close-btn" data-nav-close-btn aria-label="Close Menu">
-              <ion-icon name="close-outline"></ion-icon>
-            </button>
-          </div>
-          <div className="navbar-bottom">
-            <ul className="navbar-list">
-              <HeaderLink to="/" name="Home" />
-              <HeaderLink to="#" name="About" />
-              <HeaderLink to="#" name="Contact Us" />
-            </ul>
-          </div>
-        </nav>
-
-        
-
-      </div>
-    </div>
-
-  </header>
-  )
+            <div className="header-bottom">
+                <div className="container">
+                    <a href="/" className="logo">
+                        <AppLogo />
+                    </a>
+                    <nav className="navbar" data-navbar>
+                        <div className="navbar-top">
+                            <a href="/" className="logo">
+                                <AppLogo />
+                            </a>
+                            <button
+                                className="nav-close-btn"
+                                data-nav-close-btn
+                                aria-label="Close Menu"
+                            >
+                                <ion-icon name="close-outline"></ion-icon>
+                            </button>
+                        </div>
+                        <div className="navbar-bottom">
+                            <ul className="navbar-list">
+                                <HeaderLink to="/" name="Home" />
+                                <HeaderLink to="#" name="About" />
+                                <HeaderLink to="#" name="Contact Us" />
+                            </ul>
+                        </div>
+                    </nav>
+                    <HeaderBottom />
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
