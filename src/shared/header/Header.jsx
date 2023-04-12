aec91fdc9f4f18de1b573bd90ef4e75df1b072c8
Muhammad Awais <awaisshahid965@gmail.com>
2023-04-11 23:58:47 -0700

    added header bottom

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -36,24 +36,7 @@ function Header() {
           </div>
         </nav>
 
-        <div className="header-bottom-actions">
-          <button className="header-bottom-actions-btn" aria-label="Search">
-            <ion-icon name="search-outline"></ion-icon>
-            <span>Search</span>
-          </button>
-          <button className="header-bottom-actions-btn" aria-label="Profile">
-            <ion-icon name="person-outline"></ion-icon>
-            <span>Profile</span>
-          </button>
-          <button className="header-bottom-actions-btn" aria-label="Cart">
-            <ion-icon name="cart-outline"></ion-icon>
-            <span>Cart</span>
-          </button>
-          <button className="header-bottom-actions-btn" data-nav-open-btn aria-label="Open Menu">
-            <ion-icon name="menu-outline"></ion-icon>
-            <span>Menu</span>
-          </button>
-        </div>
+        
 
       </div>
     </div>
