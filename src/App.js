c2d5d38f11ae4ce59164273e830cd977dd4d3a2e
Muhammad Awais <awaisshahid965@gmail.com>
2023-05-04 13:54:24 -0700

    fix - screen move to top on login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Header from "./shared/components/header/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import Footer from "./shared/components/footer/Footer";
 import BuildersPage from "./pages/builders";
 import AuthContainer from "./context/auth/AuthContainer";
 import Auth from "./pages/auth/Auth";
+import useBackToTop from "./shared/hooks/useBackToTop";
+import { useEffect } from "react";
+
+const BackToTop = () => {
+  const location = useLocation();
+  const backToTop = useBackToTop();
+  useEffect(() => {
+    backToTop();
+  }, [backToTop, location]);
+  return null;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContainer>
         <Header />
+        <BackToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/builders" element={<BuildersPage />} />
